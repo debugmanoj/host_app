65221fa99d903f6209dd179e33b997e308cd800c
debugmanoj <manojkumarwork01@gmail.com>
2024-12-30 20:47:48 +0530

    Added env files

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,26 @@
-// vite.config.js in host-app
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "host-app",
-      remotes: {
-        client_app: "https://client-app-kappa-snowy.vercel.app/assets/remoteEntry.js",
-      },
-      shared: ["react"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    minify: false,
-    cssCodeSplit: false,
-  },
+  console.log("VITE_CLIENT_APP_URL", process.env.VITE_CLIENT_APP_URL); // Use process.env here
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "host_app",
+        remotes: {
+          client_app: process.env.VITE_CLIENT_APP_URL,
+        },
+        shared: ["react"],
+      }),
+    ],
+    build: {
+      minify: mode === "production",
+      target: mode === "production" ? "esnext" : "es2015",
+      cssCodeSplit: mode === "production",
+    },
+  };
 });
