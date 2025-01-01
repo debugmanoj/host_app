fa505c563d74c4577d1bcd4c22639798577c63b8
debugmanoj <manojkumarwork01@gmail.com>
2025-01-01 16:42:39 +0530

    dsf

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,31 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+
 import fs from "fs";
 import path from "path";
 
-// Read package.json dependencies
-const packageJson = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "package.json"), "utf-8")
-);
-
-// Extract dependencies to be shared
-const sharedDependencies = Object.keys(packageJson.dependencies).map(dep => ({
-  [dep]: {
-    singleton: true,
-    requiredVersion: packageJson.dependencies[dep],
-  },
-}));
-
-// Flatten the array of shared dependencies
-const shared = Object.assign({}, ...sharedDependencies);
 
 // Load environment variables
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+   // Ensure the package.json file is being read correctly
+   const packageJson = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "package.json"), "utf-8")
+  );
+  
+  // Create a shared dependencies object
+  const sharedDependencies = Object.keys(packageJson.dependencies).reduce(
+    (acc, dep) => {
+      acc[dep] = {
+        singleton: true,
+        requiredVersion: packageJson.dependencies[dep],
+      };
+      return acc;
+    },
+    {}
+  );
 
   console.log(
     "process.env.VITE_CLIENT_APP_URL: ",
@@ -38,7 +41,7 @@ export default defineConfig(({ mode }) => {
           client_app: process.env.VITE_CLIENT_APP_URL,
           calc_app: process.env.VITE_CALC_APP_URL,
         },
-        shared, // Share all dependencies from package.json
+        shared: sharedDependencies, // Share all dependencies from package.json
       }),
     ],
     server: {
